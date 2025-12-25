0b1e7abb70a0d3837dc070679a6f3843435b5481
Spaghetti <stefanomocci92@gmmail.com>
2025-12-25 10:09:08 +0800

    Add GitHub Pages deployment workflow and update site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://github.com/amplifycreativelab",
+  site: "https://amplifycreativelab.github.io",
   base: "/pizza-takeaway-demo/",
 
   vite: {
